e7a0541f045ed695a5dedbdf19d93005e0cc96ab
RziankinS <98005611+RziankinS@users.noreply.github.com>
2024-10-18 23:54:36 +1000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "yandex_cloud_id" {
+  default = "b1ggp2bhrul82jpvs4rt"
+}
+
+variable "yandex_folder_id" {
+  default = "b1gh1p257m29u8c4p09p"
+}
+
+variable "zone" {
+  default = "ru-central1-a"  
+}
